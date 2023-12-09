6815ca2e86f4eba3e01ba43259ebee53dc24bdb2
Piotr Kaznowski <piotr@kazno.dev>
2023-12-09 10:28:01 +0100

    Day 9: moved parsing ints to tools

--- day09/solution.clj
+++ day09/solution.clj
@@ -1,9 +1,6 @@
 (ns day09.solution
-  (:require [tools :refer [file->lines]]
+  (:require [tools :refer [file->lines str->ints]]
             [clojure.string :refer [split-lines]]))
-
-(defn parse [lines]
-  (->> lines (map #(re-seq #"-*\d+" %)) (map (partial map parse-long))))
 
 (defn reductor [line]
   (->> line (partition 2 1) (map (fn [[a b]] (- b a)))))
@@ -15,7 +12,7 @@
        (map last) reverse (reduce + 0)))
 
 (defn -main [day]
-  (let [input (->> day file->lines parse)]
+  (let [input (day file->lines (map str->ints))]
     {:part1 (transduce (map predict) + input)
      :part2 (transduce (comp (map reverse) (map predict)) + input)}))
 
@@ -23,7 +20,7 @@
   (let [test-input "0 3 6 9 12 15
 1 3 6 10 15 21
 10 13 16 21 30 45"
-        input (->> test-input split-lines parse)]
+        input (->> test-input split-lines (map str->ints))]
     (assert (= 114 (transduce (map predict) + input)))
     (assert (= 2 (transduce (comp (map reverse) (map predict)) + input))))
   )
--- tools.clj
+++ tools.clj
@@ -1,6 +1,20 @@
 (ns tools
-  (:require [clojure.string :refer [split-lines]]))
+  (:require [clojure.string :refer [split-lines]]
+            [clojure.test :refer [deftest testing is run-tests]]))
 
 (defn file->str [dir] (slurp (str dir "/" "input.txt")))
 (defn file->lines [dir] (split-lines (file->str dir)))
+(defn str->ints [s] (->> s (re-seq #"-*\d+") (map parse-long)))
 
+
+(comment
+  (deftest str->ints-test
+    (testing "transforms string into list of ints"
+      (is (= '(1 3 2 4) (str->ints "1 asd 3 (QWER 2: 4)."))))
+    (testing "is negative numbers aware"
+      (is (= '(-1 3 -2 4) (str->ints "-1 3 -2 4"))))
+    (testing "returns empty list when no ints found"
+      (is (= () (str->ints "no ints")))))
+
+  (assert (->> (run-tests 'tools) :fail zero?))
+  )
