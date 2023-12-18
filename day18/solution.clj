b69c5702b5c70548efcbde3adc0b604d01018daf
Piotr Kaznowski <piotr@kazno.dev>
2023-12-18 22:43:07 +0100

    Day 18: refactor later

---
+++ day18/solution.clj
@@ -0,0 +1,54 @@
+(ns day18.solution
+  (:require [tools :refer [file->lines manhattan]]
+            [clojure.string :refer [split-lines]]))
+
+(defn parse [l]
+  (let [[_ dir1 num _ hex dir2] (re-matches #"(\w) (\d+) \(#((\w{5})(\w))\)" l)]
+    {:1 [dir1 (parse-long num)]
+     :2 [({"0" "R" "1" "D" "2" "L" "3" "U"} dir2) (read-string (str "0x" hex))]}))
+
+(defn edges [input ]
+  (reduce
+   (fn [acc [d n _]]
+     (conj acc
+           (let [[y x] (peek acc)]
+             (case d
+               "R" [y (+ x n )]
+               "D" [(+ y n) x]
+               "L" [y (- x n)]
+               "U" [(- y n) x]))))
+   [[0 0]] input))
+
+;; shoelace formula: https://en.wikipedia.org/wiki/Shoelace_formula
+(defn lava-vol [input]
+  (let [pairs (partition 2 1 input)
+        shoelace (reduce (fn [acc [[y x] [y' x']]] (+ acc (/ (- (* y' x) (* y x')) 2))) 0 pairs)
+        circum (reduce + (map #(apply manhattan %) pairs))]
+    (inc (+ shoelace (/ circum 2)))))
+
+(defn -main [day]
+  (let [solve-with #(->> day file->lines (map parse) % edges lava-vol)]
+    {:part1 (solve-with #(map :1 %))
+     :part2 (solve-with #(map :2 %))}))
+
+
+(comment
+  (let [test-input "R 6 (#70c710)
+D 5 (#0dc571)
+L 2 (#5713f0)
+D 2 (#d2c081)
+R 2 (#59c680)
+D 2 (#411b91)
+L 5 (#8ceee2)
+U 2 (#caa173)
+L 1 (#1b58a2)
+U 2 (#caa171)
+R 2 (#7807d2)
+U 3 (#a77fa3)
+L 2 (#015232)
+U 2 (#7a21e3)
+"
+        solve-with #(->> test-input split-lines (map parse) % edges lava-vol)]
+    (assert (= 62 (solve-with (partial map :1))))
+    (assert (= 952408144115 (solve-with (partial map :2)))))
+  )
